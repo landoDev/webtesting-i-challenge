f107794df0d131e5fdcfba8315405050670fb9d4
Landon Turner <lturner@teamworks.com>
2020-04-22 14:23:15 -0400

    initial commit. adds package js, skeleton of tests and repair test todos

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer.js', () => {
+
+    // describe('succeed()', () => {
+    //     it.todo('')
+    // })
+
+    // describe('fail()', () => {
+        
+    // })
+
+    describe('repair()', () => {
+        it.todo('should have an item object with name, durability and enhancement properties')
+        it.todo('durability property should be less than 100')
+        it.todo('should return a new item')
+        it.todo('durability property should not exceed 100')
+    })
+
+    // stretch
+    // describe('get()', () => {
+        
+    // })
+})
