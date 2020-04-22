9d7ce398777733ec18ec128cb02306743f6710ec
Landon Turner <lturner@teamworks.com>
2020-04-22 16:25:11 -0400

    adds object property test on repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,4 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
 describe('enhancer.js', () => {
 
@@ -11,12 +11,22 @@ describe('enhancer.js', () => {
     // })
 
     describe('repair()', () => {
-        it.todo('should have an item object with name, durability and enhancement properties')
+        it.todo('should throw an error when passed null or undefined argument')
+        it('should have only accept an object with name, durability and enhancement properties', () => {
+            const item = {
+                name: 'Mjolnir',
+                durability: 80,
+                enhancement: 20
+            }
+            const repairedItem = repair(item)
+            expect(repairedItem).toHaveProperty('name');
+            expect(repairedItem).toHaveProperty('durability');
+            expect(repairedItem).toHaveProperty('enhancement');
         it.todo('durability property should be less than 100')
         it.todo('should return a new item')
         it.todo('durability property should not exceed 100')
+        })
     })
-
     // stretch
     // describe('get()', () => {
         
